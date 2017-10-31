d5e9ebb9784db56319568b8e56b594049b905d38
Shane Moo <obries6@rpi.edu>
2017-10-31 13:06:58 -0400

    Something compiling. Error with free(arg)

--- hw3.c
+++ hw3.c
@@ -11,6 +11,9 @@
 
 #include "hw3.h"
 
+//gcc -Wall -Werror hw3.c -pthread
+//./a.out testfiles00 5 hw3-output00.txt
+
 //make a STRUCT here???
 
 void* threadCall(void* arg);
@@ -82,96 +85,97 @@ void getFiles(char*** directory, char* location, int* numFiles) {
 
 void handleWords(char* buff) {
 	printf("buff: %s\n", buff);
+	fflush(NULL);
 }
 
 void readFile(char* file) {
-	int currentbuffsize = 0;
-	char* totalBuffer;
 
-	for( int i = 0; i < (*numFiles); i++ ) {
-		char* buff = NULL;
-		FILE* fp = fopen(file, "r");
+	printf("INSIDE READFILE()\n");
+	fflush(NULL);
 
-		if (fp == NULL) {
-			perror("ERROR: fopen() failed RIP");
-			//break;
-			continue;
-		}
-		if (fp != NULL) {
-			#if DEBUG_MODE
-				printf("reading %s\n", file);
-			#endif
-			//go to the end of the file	
-			if (fseek(fp, 0L, SEEK_END) == 0) {
-				long bufsize = ftell(fp); //get size of file
-				if ( bufsize == -1 ) {
-					perror( "ERROR: ftell() failed" );
-				}
+	char* buffer = NULL;
+	FILE *fp = fopen(file, "r");
+	if (fp != NULL) {
+	    /* Go to the end of the file. */
+	    if (fseek(fp, 0L, SEEK_END) == 0) {
+	        /* Get the size of the file. */
+	        long bufsize = ftell(fp);
+	        if (bufsize == -1) { 
+	        	perror("ftell() failed");
+	        }
 
-				if ( fseek(fp, 0L, SEEK_SET) != 0 ) {
-					perror( "ERROR: fseek() failed" );
-				}
+	        /* Allocate our buffer to that size. */
+	        buffer = (char*)calloc(bufsize, sizeof(char));
 
-				size_t newLen = fread(buff, sizeof(char), bufsize, fp); //read into memory
-				if ( ferror(fp) != 0 ) {
-					perror(" ERROR: fread() failed" );
-				}
-				else {
-					buff[newLen++] = '\0';
-				}
-				handleWords(buff);
-			}
-			fclose(fp);
-		}
-
-		free(buff);
-		buff = NULL;
+	        /* Go back to the start of the file. */
+	        if (fseek(fp, 0L, SEEK_SET) != 0) { 
+	        	perror("fseek() failed");
+	        }
+	        /* Read the entire file into memory. */
+	        size_t newLen = fread(buffer, sizeof(char), bufsize, fp);
+	        if (newLen == 0) {
+	            perror("fread() failed");
+	        } 
+	        else {
+	            buffer[++newLen] = '\0'; /* Just to be safe. */
+	            handleWords(buffer);
+	        }
+	    }
+	    fclose(fp);
 	}
+	free(buffer);
 }
+
 
 // void critical_section() {
 
 // }
 
 //for each thread call, we need just the file name
-void threadCall(void* arg) {
-	char* t = *(char**)arg;
-	free(arg);
+void* threadCall(void* arg) {
+	char* file = *(char**)arg;
+	//free(arg);
 
-	printf("THREAD %d: I'm going to send each file to function readFile()", (int)pthread_self());
+	printf("THREAD %u: I'm going to send each file: %s to function readFile()\n", (unsigned int)pthread_self(), file);
+	fflush(NULL);
 
-	readFile
+	readFile(file);
+
+	//return the TID value
+	unsigned int* x = malloc(sizeof(unsigned int));
+	*x = pthread_self();
+	printf("THREAD %u: Exiting...\n", (unsigned int)pthread_self());
+	pthread_exit(x);
 }
 
-void bufferRun(char** directory, int numFiles) {
+void bufferRun(char** directory, unsigned int numFiles) {
 	pthread_t tid[numFiles];
 	int i, rc;
 
-	int* t;
-
-	for (i = 0; i < numFiles, i++) {
-		rc = pthread_create( &tid[i], NULL, threadCall, &(directory[i]));
+	for (i = 0; i < numFiles; i++) {
+		rc = pthread_create( &tid[i], NULL, threadCall, (void*)&(directory[i]));
 		if (rc != 0) {
 			fprintf(stderr, "MAIN: Could not create thread\n");
+			fflush(NULL);
 		}
 	}
 
-	for (i = 0; i < numFiles, i++) {
+	for (i = 0; i < numFiles; i++) {
 		unsigned int* x;
 		rc = pthread_join(tid[i], (void**)&x);
 
-		if (rc != ) {
+		if (rc != 0) {
 			fprintf(stderr, "MAIN: Could not join thread\n");
+			fflush(NULL);
 		}
 
 		printf("MAIN: Joined child thread %u, which returned %u\n", (unsigned int)tid[i], *x);
+		fflush(NULL);
 		free(x);
 	}
 
 	printf("MAIN: All threads terminated successfully.\n");
 	fflush(NULL);
-
-	return EXIT_SUCCESS
 }
 
 int main(int argc, char* argv[]) {
@@ -198,6 +202,7 @@ int main(int argc, char* argv[]) {
 	i = 0;
 	while (i < numFiles) {
 		printf("files: %s\n", directory[i]);
+		fflush(NULL);
 		i++;
 	}
 
